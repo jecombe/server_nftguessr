733d937cccb9a27204d87a5fb0da9477d2b9754b
jeremy <jeremcombe@hotmail.fr>
2023-11-02 15:54:19 +0100

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       path: "/home/ubuntu/server/",
       "pre-deploy-local": "", // Commande pré-déploiement locale
       "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && pm2 startOrRestart ecosystem.config.js --env production", // Commandes post-déploiement
+        "source ~/.nvm/nvm.sh && npm install && pm2 startOrRestart ./ecosystem.config.js --env production", // Commandes post-déploiement
       "pre-setup": "", // Commande pré-config du serveur
       ssh_options: "PasswordAuthentication=yes", // Option pour autoriser l'authentification par mot de passe
       key: "~/.ssh/id_rsa", // Chemin vers votre clé RSA privée
